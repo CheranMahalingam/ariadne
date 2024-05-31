1c4d605c62dcddecedc94fa8f490a2b49c2a2f91
Cheran Mahalingam <cheranm@outlook.com>
2024-05-30 20:09:28 -0700

    build: initialize ros2 humble vslam package

---
+++ vslam/src/vslam_node.cpp
@@ -0,0 +1,13 @@
+#include "cuda_vslam_ros/vslam.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+#include <memory>
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  rclcpp::spin(std::make_shared<vslam::VSLAMNode>(options));
+  rclcpp::shutdown();
+  return 0;
+}
