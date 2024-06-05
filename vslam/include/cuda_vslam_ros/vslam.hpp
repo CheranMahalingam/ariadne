1fb5e727d5d6afe4363742b5468c1a5d7446ba63
Cheran Mahalingam <cheranm@outlook.com>
2024-06-04 23:08:38 -0700

    feat(tracking): design ORB feature extractor

--- vslam/include/cuda_vslam_ros/vslam.hpp
+++ vslam/include/cuda_vslam_ros/vslam.hpp
@@ -1,7 +1,12 @@
 #ifndef CUDA_VSLAM_ROS__VSLAM_NODE_HPP_
 #define CUDA_VSLAM_ROS__VSLAM_NODE_HPP_
 
-#include <rclcpp/rclcpp.hpp>
+#include "tracking/feature_extractor.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+#include <memory>
 
 namespace vslam
 {
@@ -10,6 +15,23 @@ class VSLAMNode : public rclcpp::Node
 {
 public:
   VSLAMNode(const rclcpp::NodeOptions & options);
+
+private:
+  void rgbImageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg) const;
+
+  // ROS configurable parameters
+  int orb_scale_pyramid_levels_;
+  float orb_pyramid_scale_factor_;
+  int orb_num_features_;
+  int orb_initial_FAST_threshold_;
+  int orb_min_FAST_threshold_;
+  int camera_width_;
+  int camera_height_;
+
+  std::unique_ptr<FeatureExtractor> extractor_;
+
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr rgb_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_sub_;
 };
 
 }  // vslam
--- vslam/src/vslam.cpp
+++ vslam/src/vslam.cpp
@@ -1,9 +1,48 @@
 #include "cuda_vslam_ros/vslam.hpp"
 
+#include <cv_bridge/cv_bridge.h>
+
 namespace vslam
 {
 
+using std::placeholders::_1;
+
 VSLAMNode::VSLAMNode(const rclcpp::NodeOptions & options)
-: Node("vslam", options) {}
+: Node("vslam", options)
+{
+  this->declare_parameter("orb_scale_pyramid_levels", 8);
+  this->declare_parameter("orb_pyramid_scale_factor", 1.2);
+  this->declare_parameter("orb_num_features", 2000);
+  this->declare_parameter("orb_initial_FAST_threshold", 20);
+  this->declare_parameter("orb_min_FAST_threshold", 7);
+  this->declare_parameter("camera_width", 1920);
+  this->declare_parameter("camera_height", 1080);
+
+  this->get_parameter("orb_scale_pyramid_levels", orb_scale_pyramid_levels_);
+  this->get_parameter("orb_pyramid_scale_factor", orb_pyramid_scale_factor_);
+  this->get_parameter("orb_num_features", orb_num_features_);
+  this->get_parameter("orb_initial_FAST_threshold", orb_initial_FAST_threshold_);
+  this->get_parameter("orb_min_FAST_threshold", orb_min_FAST_threshold_);
+  this->get_parameter("camera_width", camera_width_);
+  this->get_parameter("camera_height", camera_height_);
+
+  extractor_ = std::make_unique<FeatureExtractor>(
+    orb_scale_pyramid_levels_,
+    orb_pyramid_scale_factor_,
+    orb_num_features_,
+    orb_initial_FAST_threshold_,
+    orb_min_FAST_threshold_);
+
+  rgb_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+    "rgb_image", 10, std::bind(&VSLAMNode::rgbImageCallback, this, _1));
+}
+
+void VSLAMNode::rgbImageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg) const
+{
+  auto cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+  std::vector<cv::KeyPoint> key_points;
+  cv::Mat descriptors;
+  extractor_->ComputeFeatures(cv_ptr->image, key_points, descriptors);
+}
 
 }  // vslam
