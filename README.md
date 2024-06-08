40557746a779865fc66de849392fbb9691c9ad70
Cheran Mahalingam <cheranm@outlook.com>
2024-06-07 19:59:36 -0700

    build(docker): set up docker dev environment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cuda_vslam_ros
-A ROS2 package implementing a hardware-accelerated visual SLAM system based on ORB-SLAM for RGB-D cameras
+A ROS2 package implementing a hardware-accelerated visual SLAM system based on ORB-SLAM for RGB-D cameras.
+
+## Development
+All development occurs in a reproducible environment built using Docker. To build a dev image and
+exec into the container run,
+```
+./scripts/setup-dev-env.sh
+```
---
+++ scripts/setup-dev-env.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+running_containers=$(docker ps |
+  { grep -c "atlas-robot" || test $? = 1; })
+if [[ "$running_containers" -eq 0 ]]
+then
+  exited_containers=$(docker ps -a |
+    { grep -c "atlas-robot" || test $? = 1; })
+  if [[ "$exited_containers" -gt 0 ]]
+  then
+    echo "Killing the atlas-robot container..."
+    # Filter for all containers with name "atlas-robot".
+    docker ps -a |
+      awk '{if ($NF == "atlas-robot") print $1}' |
+      xargs docker rm
+  fi
+  echo "Building the atlas image..."
+  if [[ "$(uname -s)" == "Darwin" ]]
+  then
+    HOST_UID=1000
+    HOST_GID=1000
+  else
+    HOST_UID=$(id -u)
+    HOST_GID=$(id -g)
+  fi
+  docker build -t atlas . \
+    --build-arg HOST_UID="$HOST_UID" \
+    --build-arg HOST_GID="$HOST_GID" \
+    --target devel
+  echo "Initializing the atlas-robot container..."
+  docker run -it \
+    --user "$HOST_UID":"$HOST_GID" \
+    --name atlas-robot \
+    --mount type=bind,source="$(pwd)",target=/home/docker/ros2_ws \
+    atlas:latest
+else
+    echo "Found an instance of the atlas image running, attaching..."
+    docker attach atlas-robot
+fi
