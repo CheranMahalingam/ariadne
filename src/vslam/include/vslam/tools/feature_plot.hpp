4c722869bb85d8773b9b84ca2489be6c456e6327
Cheran Mahalingam <cheranm@outlook.com>
2024-06-18 15:34:19 -0700

    feat(tools): generate annotated plots with extracted features

---
+++ src/vslam/include/vslam/tools/feature_plot.hpp
@@ -0,0 +1,18 @@
+#ifndef VSLAM__FEATURE_PLOT_HPP_
+#define VSLAM__FEATURE_PLOT_HPP_
+
+#include <filesystem>
+
+class FeaturePlot
+{
+public:
+  FeaturePlot(std::string input_dir, std::string output_dir);
+
+  void Plot();
+
+private:
+  std::filesystem::path input_dir_;
+  std::filesystem::path output_dir_;
+};
+
+#endif  // VSLAM__FEATURE_PLOT_HPP_
